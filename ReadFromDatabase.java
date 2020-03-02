633aee9dc2f15ad62e0391909944802e07c8acc6
summerargo <36552781+summerargo@users.noreply.github.com>
2020-03-02 17:32:35 -0500

    Update ReadFromDatabase.java

--- ReadFromDatabase.java
+++ ReadFromDatabase.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 
 public class ReadFromDatabase {
     //Constructor
-    public ReadFromDatabase(File file) {
+    public ReadFromDatabase(File DataBase) {
     }
 
     //Method reads from database file
