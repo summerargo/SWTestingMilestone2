4e5d6b81ea7df7a814dfda612f78550f1d8f5f08
summerargo <36552781+summerargo@users.noreply.github.com>
2020-03-02 17:32:05 -0500

    Update SearchDatabase.java

--- SearchDatabase.java
+++ SearchDatabase.java
@@ -3,11 +3,11 @@ import java.sql.Connection;
 
 public class SearchDatabase {
     //Constructor
-    public SearchDatabase(File file) {
+    public SearchDatabase(File DataBase) {
     }
 
     //Method for searching through the database information correlating with the search
-    public Connection SearchThroughFile( String search) {
+    public Connection SearchThroughFile(String search) {
         return null;
     }
 }
