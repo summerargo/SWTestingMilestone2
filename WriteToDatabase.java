63ab0154e467e552afda5accdf339abe3bb6f1dd
summerargo <36552781+summerargo@users.noreply.github.com>
2020-03-02 14:52:12 -0500

    Update WriteToDatabase.java

--- WriteToDatabase.java
+++ WriteToDatabase.java
@@ -7,17 +7,27 @@ public class WriteToDatabase {
     }
 
     //Method that adds a project to the database
-    public Connection AddProjectToDB(String name, int number, string requirements) {
+    public Connection AddProjectToDB(String project_name, int project_ID, String requirements) {
         return null;
     }
-    
+
     //Method that adds a course to the database
-    public Connection AddCourseToDB(String name, int number, string prerequisite) {
+    public Connection AddCourseToDB(String course_name, int course_ID, String prerequisite) {
+        return null;
+    }
+
+    //Method that adds a student to the database
+    public Connection AddStudentToDB(String student_name, int student_ID, String skills[], String classification) {
+        return null;
+    }
+
+    //Method that adds a faculty to the database
+    public Connection AddFacultyToDB(String faculty_name, int faculty_ID, String position, Double salary) {
         return null;
     }
 
     //Method that removes from database
-    public Connection RemoveFromDB(int id) {
+    public Connection RemoveFromDB(int ID) {
         return null;
     }
 }
